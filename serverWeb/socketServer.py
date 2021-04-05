5c76c3373a252ce4ba04bcb64c449f7069e6bc01
son-of-win <hackeranonymous1310@gmail.com>
2021-04-05 08:30:34 +0700

    update fix

--- serverWeb/socketServer.py
+++ serverWeb/socketServer.py
@@ -3,27 +3,32 @@ import sys
 
 hostname = "10.90.80.22"
 port = 1310
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind((hostname, port))
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((hostname, 1310))
 serverSocket.listen(1)
 while True:
     print('ready to server.....')
     connectionSocket, addr = serverSocket.accept()
     try:
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(4096)
         print(message)
         filename = message.split()[1]
-        if not filename:
-            f = open("index.html")
-        else:
-            f = open(filename[1:])
+        # if not filename:
+        #     f = open("index.html")
+        # else:
+        #     f = open(filename[1:].decode('utf-8'))
+        f = open("C:\\Users\\Admin\\Documents\\network\\btl_network_socket\\serverWeb\\index.html","rb")
+
         outputdata = f.read()
         f.close()
 
-        connectionSocket.send(b'HTTP/1.0 200 OK\r\n\r\r\n')
-        for i in range(0, len(outputdata)):
-            print(outputdata[i])
-            connectionSocket.send(bytes(outputdata[i],'utf-8'))
+        connectionSocket.send(b'HTTP/1.1 200 OK\r\n\r\r\n')
+        header = "'HTTP/1.1 200 OK\r\n\r\r\n"
+        content = "Context-Type: text/html \n\n"
+        data = header.encode('utf-8')
+        data += content.encode("utf-8")
+        data = data + outputdata
+        connectionSocket.send(data)
         connectionSocket.close()
 
     except IOError:
